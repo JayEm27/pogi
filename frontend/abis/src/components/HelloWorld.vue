98410aaf4ac1722e5eb69091de6c7483af49ebb1
JayEm27 <johnmarkabis27@gmail.com>
2025-08-11 22:56:13 -0700

    up

---
+++ frontend/abis/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+#integer 
+
+a = 10
+b = 3
+
+sum = a + b
+product = a * b
+quotient = a // b  # Integer division
+remainder = a % b
+
+print("Sum:", sum)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Remainder:", remainder)
